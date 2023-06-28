a105b59c2651896e2d9d309c3bba08b59dad10c6
nickrandolph <nick@builttoroam.com>
2023-06-28 15:51:48 +1000

    fix: Adding total results to mvux

--- Mvux/MvvmVersusMvux/Presentation/MainModel.cs
+++ Mvux/MvvmVersusMvux/Presentation/MainModel.cs
@@ -10,15 +10,22 @@ namespace MvvmVersusMvux.Presentation
             _client = client;
         }
 
-        public IState<string> SearchText => State<string>.Value(this,()=>"xxx");
+        public IState<string> SearchText => State<string>.Value(this, () => "xxx");
 
         // public IListFeed<Result> Movies =>
         //ListFeed.Async(
         //    async ct => (await _client.GetMovies(await SearchText ?? string.Empty, ct)).Results);
 
+        public IState<int> TotalResults => State<int>.Value(this, () => 0);
+
         public IListFeed<Result> Movies =>
             ListFeed.AsyncPaginated<Result>(
-                async (pageRequest, ct) => (await _client.GetMovies(await SearchText ?? string.Empty, (int)pageRequest.Index + 1, ct)).Results);
+                async (pageRequest, ct) =>
+                {
+                    var movies = await _client.GetMovies(await SearchText ?? string.Empty, (int)pageRequest.Index + 1, ct);
+                    _ = TotalResults.Set(movies.TotalResults, ct);
+                    return movies.Results;
+                });
 
         //public IListFeed<Result> Movies =>
         //    SearchText.SelectAsync(
